d90778a9a14efab2c12bd8f85b421bb4c29550ba
ahanuszczak <hanyhusajn@gmail.com>
2023-03-10 08:45:20 +0100

    GameViewModel

---
+++ app/src/main/java/com/hanuszczak/guessinggame/GameViewModel.kt
@@ -0,0 +1,53 @@
+package com.hanuszczak.guessinggame
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    val words = listOf("android", "activity", "fragment")
+    val secretWod = words.random().uppercase()
+    var secretWordDisplay = ""
+    var correctGuesses = ""
+    var incorrectGuesses = ""
+    var livesLeft = 8
+
+    init {
+        secretWordDisplay = deriveSecretWordDisplay()
+    }
+
+    fun deriveSecretWordDisplay() : String {
+        var display = ""
+        secretWod.forEach {
+            display += checkLetter(it.toString())
+        }
+        return display
+    }
+
+    fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
+        true -> str
+        false -> "_"
+    }
+
+    fun makeGuess(guess: String) {
+        if (guess.length == 1) {
+            if (secretWod.contains(guess)) {
+                correctGuesses += guess
+                secretWordDisplay = deriveSecretWordDisplay()
+            } else {
+                incorrectGuesses += "$guess "
+                livesLeft--
+            }
+        }
+    }
+
+    fun isWon() = secretWod.equals(secretWordDisplay, true)
+
+    fun isLost() = livesLeft <= 0
+
+    fun wonLostMessage() : String {
+        var message = ""
+        if (isWon()) message = "You won!"
+        else if (isLost()) message = "You lost!"
+        message += " The word was $secretWod."
+        return message
+    }
+}
