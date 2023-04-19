b426b87303cfaa9c56220d7e3531c5f2e38c891b
ahanuszczak <hanyhusajn@gmail.com>
2023-04-19 09:49:44 +0200

    reproduce TextField

--- app/src/main/java/com/hanuszczak/guessinggame/GameFragment.kt
+++ app/src/main/java/com/hanuszczak/guessinggame/GameFragment.kt
@@ -7,11 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Observer
@@ -56,16 +55,31 @@ class GameFragment : Fragment() {
         }
         return view
     }
+
     @Composable
     private fun GameFragmentContent(viewModel: GameViewModel) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            FinishGameButton {
-                viewModel.finishGame()
+        val guess = remember {
+            mutableStateOf("")
+        }
+        Column(modifier = Modifier.fillMaxWidth()) {
+            EnterGuess(guess = guess.value) { guess.value = it }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                FinishGameButton {
+                    viewModel.finishGame()
+                }
             }
         }
+    }
+
+    @Composable
+    fun EnterGuess(guess: String, changed: (String) -> Unit) {
+        TextField(
+            value = guess,
+            onValueChange = changed,
+            label = { Text(text = "Guess a letter") })
     }
 
     @Composable
