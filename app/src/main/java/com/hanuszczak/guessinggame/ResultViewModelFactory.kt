4d0f00e8c285af28416688d80d0ae919a5cea257
ahanuszczak <hanyhusajn@gmail.com>
2023-04-20 23:09:03 +0200

    @Suppress(UNCHECKED_CAST)

--- app/src/main/java/com/hanuszczak/guessinggame/ResultViewModelFactory.kt
+++ app/src/main/java/com/hanuszczak/guessinggame/ResultViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.hanuszczak.guessinggame
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+@Suppress("UNCHECKED_CAST")
 class ResultViewModelFactory(private val finalResult: String) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ResultViewModel::class.java))
